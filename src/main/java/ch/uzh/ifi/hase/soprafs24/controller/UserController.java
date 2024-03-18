42c64d639cd0127188fa27cf4ef9f12d85599f3a
SopraPH <sopraphilip@gmail.com>
2024-03-18 18:19:13 +0100

    first

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/controller/UserController.java
@@ -0,0 +1,117 @@
+package ch.uzh.ifi.hase.soprafs24.controller;
+
+import ch.uzh.ifi.hase.soprafs24.entity.User;
+import ch.uzh.ifi.hase.soprafs24.rest.dto.UserGetDTO;
+import ch.uzh.ifi.hase.soprafs24.rest.dto.UserPostDTO;
+import ch.uzh.ifi.hase.soprafs24.rest.mapper.DTOMapper;
+import ch.uzh.ifi.hase.soprafs24.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.http.ResponseEntity;
+
+/**
+ * User Controller
+ * This class is responsible for handling all REST request that are related to
+ * the user.
+ * The controller will receive the request and delegate the execution to the
+ * UserService and finally return the result.
+ */
+@RestController
+public class UserController {
+
+  private final UserService userService;
+
+  UserController(UserService userService) {
+    this.userService = userService;
+  }
+
+  @GetMapping("/users")
+  @ResponseStatus(HttpStatus.OK)
+  @ResponseBody
+  public List<UserGetDTO> getAllUsers() {
+    // fetch all users in the internal representation
+    List<User> users = userService.getUsers();
+    List<UserGetDTO> userGetDTOs = new ArrayList<>();
+
+    // convert each user to the API representation
+    for (User user : users) {
+      userGetDTOs.add(DTOMapper.INSTANCE.convertEntityToUserGetDTO(user));
+    }
+    return userGetDTOs;
+  }
+
+  @PostMapping("/users")
+  @ResponseStatus(HttpStatus.CREATED)
+  @ResponseBody
+  public UserGetDTO createUser(@RequestBody UserPostDTO userPostDTO) {
+    // convert API user to internal representation
+    User userInput = DTOMapper.INSTANCE.convertUserPostDTOtoEntity(userPostDTO);
+
+    // create user
+    User createdUser = userService.createUser(userInput);
+    // convert internal representation of user back to API
+    return DTOMapper.INSTANCE.convertEntityToUserGetDTO(createdUser);
+  }
+
+  @GetMapping("/users/{userId}")
+  @ResponseStatus(HttpStatus.OK)
+  @ResponseBody
+  public ResponseEntity<UserGetDTO> getUserById(@PathVariable Long userId) {
+    User user = userService.getUserById(userId);
+    
+    // If user not found, return a 404 Not Found response
+    if (user == null) {
+      return ResponseEntity.notFound().build();
+    }
+    
+    // Convert the user to DTO
+    UserGetDTO userGetDTO = DTOMapper.INSTANCE.convertEntityToUserGetDTO(user);
+    
+    // Return the DTO in the response body with status 200 OK
+    return ResponseEntity.ok(userGetDTO);
+  }
+
+  @PostMapping("/login")
+  public ResponseEntity<?> login(@RequestBody User loginUser) {
+
+    User authenticatedUser = userService.checkUserCredentials(loginUser);
+
+    if (authenticatedUser != null) {
+      return ResponseEntity.ok(authenticatedUser);
+    } else {
+      // Return a 401 Unauthorized response
+      return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid username or password");
+    }
+  }
+
+  @PutMapping("/logout")
+  public ResponseEntity<?> logout(@RequestBody User userId) {
+    User statusUser = userService.updateUserStatus(userId);
+
+    // Check if the user was successfully updated
+    if (statusUser != null) {
+      return ResponseEntity.ok().build();
+    } else {
+      // User not found, return 404 Not Found error
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No user found!");
+    }
+  }
+
+  @PutMapping("/users/{userId}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  @ResponseBody
+  public ResponseEntity<?> updateUserbyId(@PathVariable Long userId, @RequestBody User request) {
+
+    User userToUpdate = userService.updateUserById(userId, request.getUsername(), request.getBirthday());
+    
+    // If user not found, return a 404 Not Found response
+    if (userToUpdate == null) {
+      return ResponseEntity.notFound().build();
+    }
+    // If user found, return a 204 No Content response    
+    return ResponseEntity.noContent().build();  
+  }
+}
---
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/service/UserService.java
@@ -0,0 +1,131 @@
+package ch.uzh.ifi.hase.soprafs24.service;
+
+import ch.uzh.ifi.hase.soprafs24.constant.UserStatus;
+import ch.uzh.ifi.hase.soprafs24.entity.User;
+import ch.uzh.ifi.hase.soprafs24.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+import java.util.Optional;
+
+/**
+ * User Service
+ * This class is the "worker" and responsible for all functionality related to
+ * the user
+ * (e.g., it creates, modifies, deletes, finds). The result will be passed back
+ * to the caller.
+ */
+@Service
+@Transactional
+public class UserService {
+
+  private final Logger log = LoggerFactory.getLogger(UserService.class);
+
+  private final UserRepository userRepository;
+
+  @Autowired
+  public UserService(@Qualifier("userRepository") UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
+
+  public User checkUserCredentials(User loginUser) {
+    User existingUser = userRepository.findByUsername(loginUser.getUsername());
+
+    if (existingUser != null && loginUser.getName().equals(existingUser.getName())) {
+      existingUser.setStatus(UserStatus.ONLINE);
+      return existingUser; // Credentials are valid
+    } else {
+      return null;
+    }
+  }
+
+  public User getUserById(Long userId) {
+    Optional<User> optionalUser = userRepository.findById(userId);
+    User foundUser = optionalUser.orElse(null);
+    if (foundUser != null){
+      return foundUser;
+    } else {
+      return null;
+    }
+  }
+
+  public User updateUserById(Long Id, String newUsername, LocalDate newBirthday) {
+    Optional<User> optionalUser = userRepository.findById(Id);
+    User userToUpdate = optionalUser.orElse(null);
+
+    if (userToUpdate == null) {
+      return null;
+    }
+
+    if (newUsername != "") {
+      userToUpdate.setUsername(newUsername);
+    }
+    if (newBirthday != null) {
+      userToUpdate.setBirthday(newBirthday);
+    }
+    return userRepository.save(userToUpdate);
+  }
+
+  public User updateUserStatus(User userId) {
+    Optional<User> optionalUser = userRepository.findById(userId.getId());
+    User statusUser = optionalUser.orElse(null);
+    if (statusUser != null) {
+      // Update the user's status
+      statusUser.setStatus(UserStatus.OFFLINE);
+      return statusUser;
+    } else {
+      return null; // User not found
+    }
+  }
+
+  public List<User> getUsers() {
+    return this.userRepository.findAll();
+  }
+
+  public User createUser(User newUser) {
+    newUser.setToken(UUID.randomUUID().toString());
+    newUser.setStatus(UserStatus.ONLINE);
+    newUser.setCreationDate(LocalDate.now());
+    checkIfUserExists(newUser);
+    // saves the given entity but data is only persisted in the database once
+    // flush() is called
+    newUser = userRepository.save(newUser);
+    userRepository.flush();
+
+    log.debug("Created Information for User: {}", newUser);
+    return newUser;
+  }
+
+  /**
+   * This is a helper method that will check the uniqueness criteria of the
+   * username and the name
+   * defined in the User entity. The method will do nothing if the input is unique
+   * and throw an error otherwise.
+   *
+   * @param userToBeCreated
+   * @throws org.springframework.web.server.ResponseStatusException
+   * @see User
+   */
+  private void checkIfUserExists(User userToBeCreated) {
+    User userByUsername = userRepository.findByUsername(userToBeCreated.getUsername());
+    User userByName = userRepository.findByName(userToBeCreated.getName());
+
+    String baseErrorMessage = "The %s provided %s not unique. Therefore, the user could not be created!";
+    if (userByUsername != null && userByName != null) {
+      throw new ResponseStatusException(HttpStatus.CONFLICT,
+          String.format(baseErrorMessage, "username and the password", "are"));
+    } else if (userByUsername != null) {
+      throw new ResponseStatusException(HttpStatus.CONFLICT, String.format(baseErrorMessage, "username", "is"));
+    } else if (userByName != null) {
+      throw new ResponseStatusException(HttpStatus.CONFLICT, String.format(baseErrorMessage, "password", "is"));
+    }
+  }
+}
