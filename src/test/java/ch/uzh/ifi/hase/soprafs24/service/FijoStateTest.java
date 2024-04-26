dd78c545c269e754faae8ab82ee9b002d52b80d5
kkuneva <120613597+kkuneva@users.noreply.github.com>
2024-04-26 17:54:42 +0200

    Implement testing for the game related entities. #139

--- src/test/java/ch/uzh/ifi/hase/soprafs24/service/FijoStateTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs24/service/FijoStateTest.java
@@ -1,9 +1,8 @@
 package ch.uzh.ifi.hase.soprafs24.service;
 
-import ch.uzh.ifi.hase.soprafs24.entity.Bid;
-import ch.uzh.ifi.hase.soprafs24.entity.Player;
+import ch.uzh.ifi.hase.soprafs24.entity.*;
+
 import ch.uzh.ifi.hase.soprafs24.entity.RoundState.FijoState;
-import ch.uzh.ifi.hase.soprafs24.entity.Suit;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -31,5 +30,67 @@ class FijoStateTest {
 
         assertThrows(IllegalArgumentException.class, () -> state.placeBid(bid, validBids), "Invalid bid should throw an exception");
     }
+    @Test
+    void testDudo_PlayerWins() {
+        User u1 = new User();
+        u1.setId(1L);
+        User u2 = new User();
+        u2.setId(2L);
 
+        FijoState state = new FijoState();
+        Bid currentBid = new Bid(Suit.ACE, 2L);
+        Map<Suit, Long> suitCounter = Map.of(Suit.ACE, 3L);
+        Player currentPlayer = new Player(u1);
+        Player lastPlayer = new Player(u2);
+
+        Long result = state.dudo(currentBid, suitCounter, currentPlayer, lastPlayer);
+
+        assertEquals(currentPlayer.getId(), result, "Current player should win when total amount is greater than or equal to bid amount");
+    }
+
+    @Test
+    void testDudo_LastPlayerWins() {
+        User u1 = new User();
+        u1.setId(1L);
+        User u2 = new User();
+        u2.setId(2L);
+
+        FijoState state = new FijoState();
+        Bid currentBid = new Bid(Suit.ACE, 2L);
+        Map<Suit, Long> suitCounter = Map.of(Suit.ACE, 1L);
+        Player currentPlayer = new Player(u1);
+        Player lastPlayer = new Player(u2);
+
+        Long result = state.dudo(currentBid, suitCounter, currentPlayer, lastPlayer);
+
+        assertEquals(lastPlayer.getId(), result, "Last player should win when total amount is less than bid amount");
+    }
+
+    @Test
+    void testGetValidBids_NoCurrentBid() {
+        User u1 = new User();
+        u1.setId(1L);
+        FijoState state = new FijoState();
+        Bid currentBid = new Bid(null, null);
+        Player bidder = new Player(u1);
+        Long playerSize = 3L;
+
+        List<Bid> validBids = state.getValidBids(currentBid, bidder, playerSize);
+
+        assertEquals(90, validBids.size(), "Valid bids should be generated for each suit and value from 1 to 5 times the player size");
+    }
+
+    @Test
+    void testGetNextBid_NoCurrentBid() {
+        User u1 = new User();
+        u1.setId(1L);
+        FijoState state = new FijoState();
+        Bid currentBid = new Bid(null, null);
+        Player bidder = new Player(u1);
+        Long playerSize = 3L;
+
+        Bid nextBid = state.getNextBid(currentBid, bidder, playerSize);
+
+        assertEquals(new Bid(Suit.NINE, 1L), nextBid, "Next bid should be the minimum bid when no current bid exists");
+    }
 }
